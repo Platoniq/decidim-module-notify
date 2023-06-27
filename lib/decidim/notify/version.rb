96208eddeda974c4f1f93b3ee6119cb42808a471
Marina Muñoz <marina.munoz@nazaries.com>
2023-06-27 11:59:33 +0200

    fix version.rb

--- lib/decidim/notify/version.rb
+++ lib/decidim/notify/version.rb
@@ -4,6 +4,6 @@ module Decidim
   # This holds the decidim-notify version.
   module Notify
     VERSION = "0.6"
-    COMPAT_DECIDIM_VERSION = ">= 0.27, < 0.28"
+    COMPAT_DECIDIM_VERSION = [">= 0.27.0", "< 0.28"].freeze
   end
 end
