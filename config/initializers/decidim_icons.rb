2796203cd4a634b6162ccd9bd39113f9c5b35cd0
Harrison Hassig <harrisonhassig@gmail.com>
2025-01-13 19:12:16 +0100

    add initializer for icons

---
+++ config/initializers/decidim_icons.rb
@@ -0,0 +1,2 @@
+Decidim.icons.register(name: "pencil", icon: "pencil", category: "system", description: "", engine: :core)
+Decidim.icons.register(name: "trash", icon: "trash", category: "system", description: "", engine: :core)
