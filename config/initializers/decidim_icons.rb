f1f546a6c65321fcb81de0bafbd751ec0bdd9fcb
Harrison Hassig <harrisonhassig@gmail.com>
2025-01-14 13:02:45 +0100

    add frozen string literal true

--- config/initializers/decidim_icons.rb
+++ config/initializers/decidim_icons.rb
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 Decidim.icons.register(name: "pencil-line", icon: "pencil-line", category: "system", description: "", engine: :core)
 Decidim.icons.register(name: "delete-bin-line", icon: "delete-bin-line", category: "system", description: "", engine: :core)
