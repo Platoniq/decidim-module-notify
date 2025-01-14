0527d672d381bc81c20beef6238dded7fce95b44
Harrison Hassig <harrisonhassig@gmail.com>
2025-01-14 15:11:02 +0100

    Remove decidim icons initializer

--- config/initializers/decidim_icons.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-Decidim.icons.register(name: "pencil-line", icon: "pencil-line", category: "system", description: "", engine: :core)
-Decidim.icons.register(name: "delete-bin-line", icon: "delete-bin-line", category: "system", description: "", engine: :core)
