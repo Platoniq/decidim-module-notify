84ebe8a81a1c825474693dc8d025b0a88f2b6eee
Harrison Hassig <harrisonhassig@gmail.com>
2025-01-14 12:57:16 +0100

    change icons; development app working, test app failing tests

--- config/initializers/decidim_icons.rb
+++ config/initializers/decidim_icons.rb
@@ -1,2 +1,2 @@
-Decidim.icons.register(name: "pencil", icon: "pencil", category: "system", description: "", engine: :core)
-Decidim.icons.register(name: "trash", icon: "trash", category: "system", description: "", engine: :core)
+Decidim.icons.register(name: "pencil-line", icon: "pencil-line", category: "system", description: "", engine: :core)
+Decidim.icons.register(name: "delete-bin-line", icon: "delete-bin-line", category: "system", description: "", engine: :core)
